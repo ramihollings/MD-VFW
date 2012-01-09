e4c14d172ee26a1d77d602b3542d7b69a768a545
ramihollings <ramihollingsworth@gmail.com>
2012-01-08 19:11:30 -0700

    Started Java

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 // Project: Web App Part 2
 // Rami Hollingsworth
 // Term 0112
+
+window.addEventListener("DOMContentLoaded", function(){
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	
+	var displayLink = $("displayLink");
+	displayLink.addEventLister("click", getData);
+	var clearLink = $("clear");
+	clearLink.addEventLister("click", clearLocal);
+	var save = $("submit");
+	save.addEventLister("click, saveData);
+
+});
