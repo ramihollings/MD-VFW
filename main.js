db2377c3a8d40a2980ea288fb81820c82da11d1e
ramihollings <ramihollingsworth@gmail.com>
2012-01-08 19:43:48 -0700

    added selector

--- main.js
+++ main.js
@@ -9,11 +9,31 @@ window.addEventListener("DOMContentLoaded", function(){
 		return theElement;
 	}
 	
+	function selectOwnership(){
+		var formTag = document.getElementsByTagName("form"),
+			selectListItem = $("select"),
+			makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id","groups");
+		for(var i=0, j=ownTypes.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optText = ownTypes[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectListItem.appendChild(makeSelect);
+		
+	}
+	
+	var ownTypes = ["---Do I own this Item---", "Own","Owned", "Want"];
+	selectOwnership();
+	/*
+	
 	var displayLink = $("displayLink");
 	displayLink.addEventLister("click", getData);
 	var clearLink = $("clear");
 	clearLink.addEventLister("click", clearLocal);
 	var save = $("submit");
-	save.addEventLister("click, saveData);
-
+	save.addEventLister("click, saveData");
+*/
 });
